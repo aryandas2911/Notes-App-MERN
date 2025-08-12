edb486f719d1bcdd2643b509db949cb60bc2595d
Aryan Das <aryandas2911@gmail.com>
2025-08-12 23:53:19 +0530

    Router file created

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express= require ("express");
+const notesRoutes= require("./routes/notesRoutes.js")
+
 const app= express();
 
-
+app.use("/api/notes", notesRoutes)
 
 app.listen(5000, ()=>{
     console.log("Server started on port 5000");
-})
+});
