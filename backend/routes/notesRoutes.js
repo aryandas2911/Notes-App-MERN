e070c9b53bc87679f1e452cb283e161ab80df70e
Aryan Das <aryandas2911@gmail.com>
2025-08-14 01:47:20 +0530

    Controller file created

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,22 +1,13 @@
+import { createNote, deleteNote, getAllNotes, updateNote } from "../controllers/notesController.js";
+
 const express= require("express");
 
 const router= express.Router();
 
-router.get("/",(req,res)=>{
-    res.status(200).send("You just fetched the notes");
-});
-
-router.post("/",(req,res)=>{
-    res.status(201).json({message:"Note created successfully"});
-})
-
-router.put("/:id",(req,res)=>{
-    res.status(200).json({message:"Note updated successfully"});
-});
-
-router.delete("/:id",(req,res)=>{
-    res.status(200).json({message:"Note deleted successfully"});
-});
+router.get("/", getAllNotes);
+router.post("/", createNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
 
 export default router
 
