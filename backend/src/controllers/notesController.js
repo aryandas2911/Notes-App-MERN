4f87cd8aa42fbaa451862a13d1d790ecd26ff19a
Aryan Das <aryandas2911@gmail.com>
2025-08-23 23:32:44 +0530

    Added updateNote function

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -25,12 +25,20 @@ export async function createNote (req,res){
 
 export async function updateNote (req,res){
     try {
-        
+        const {title,content}=req.body
+        const updatedNote=await Note.findByIdAndUpdate(req.params.id,{title,content})
+        if (!updatedNote) return res.status(404).json({message:"Note not found"})
+        res.status(200).json(updatedNote)
     } catch (error) {
-        
+        console.error("Error in updateNote controller", error)
+        res.status(500).json({message:"Internal server error"})
     }
 }
 
 export async function deleteNote(req,res){
-    
+    try {
+        await Note.findByIdAndDelete(req.params.id)
+    } catch (error) {
+        
+    }
 }
