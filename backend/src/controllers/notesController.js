e767e7f4f2ac4c1ff7c0d5ecceb9f17a45e9d4fd
Aryan Das <aryandas2911@gmail.com>
2025-08-23 23:00:14 +0530

    Added createNote function

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -12,14 +12,23 @@ export async function getAllNotes (req,res){
 
 export async function createNote (req,res){
     try {
-        
+        const {title,content}=req.body
+        const note= new Note({title,content})
+
+        const savedNote=await note.save()
+        res.status(201).json(savedNote)
     } catch (error) {
-        
+        console.error("Error in createNote controller", error)
+        res.status(500).json({message:"Internal server error"})
     }
 }
 
 export async function updateNote (req,res){
-    
+    try {
+        
+    } catch (error) {
+        
+    }
 }
 
 export async function deleteNote(req,res){
