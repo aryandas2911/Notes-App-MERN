69a5764b0463ad69cfcae66c548fd870548eeb30
Aryan Das <aryandas2911@gmail.com>
2025-08-23 23:45:07 +0530

    Added getNotebyId function

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,8 +1,8 @@
 import {Note} from "../models/Note.js"
 
-export async function getAllNotes (req,res){
+export async function getAllNotes (_,res){
     try {
-        const notes=await Note.find()
+        const notes=await Note.find().sort({createdAt:-1})
         res.status(200).json(notes)
     } catch (error) {
         console.error("Error in getAllNotes controller", error)
@@ -12,10 +12,11 @@ export async function getAllNotes (req,res){
 
 export async function getNotebyId (req,res){
     try {
-        const notes=await Note.find()
-        res.status(200).json(notes)
+        const note= await Note.findById(req.params.id)
+        if (!note) return res.status(404).json({message:"Note not found"})
+        res.json(note)
     } catch (error) {
-        console.error("Error in getAllNotes controller", error)
+        console.error("Error in getNotebyId controller", error)
         res.status(500).json({message:"Internal server error"})
     }
 };
